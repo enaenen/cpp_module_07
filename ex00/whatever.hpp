0bb9b7988552b8970e31d12be87263a8a0f399e0
SpaceChae <enaenen@naver.com>
2022-09-19 21:12:12 +0900

    ex00 complete

---
+++ ex00/whatever.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <iostream>
+
+template <typename T>
+void swap( T &a, T &b ) {
+	T tmp = a;
+	a = b;
+	b = tmp;
+}
+
+template <typename T>
+T &max( T &a, T &b ) {
+	return a < b ? b : a;
+}
+
+template <typename T>
+T &min( T &a, T &b ) {
+	return a < b ? a : b;
+}
+
+template <typename T>
+const T &max( const T &a, const T &b ) {
+	return a < b ? b : a;
+}
+
+template <typename T>
+const T &min( const T &a, const T &b ) {
+	return a < b ? a : b;
+}
